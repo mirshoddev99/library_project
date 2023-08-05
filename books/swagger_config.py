451279e76b522510fb31aa54919f6b4e07e1113b
mirshod <oripovmirshod9@gmail.com>
2023-08-05 19:15:31 +0500

    swagger changes

--- books/swagger_config.py
+++ books/swagger_config.py
@@ -12,6 +12,6 @@ schema_view = get_schema_view(
         license=openapi.License(name='demo license'),
     ),
     public=True,
-    permission_classes=[permissions.IsAuthenticated, ],
+    permission_classes=[permissions.AllowAny, ],
 
 )
